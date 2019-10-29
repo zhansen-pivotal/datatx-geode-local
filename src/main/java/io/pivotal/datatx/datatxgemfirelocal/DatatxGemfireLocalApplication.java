fc42dd2591188c3dd3d084ae91bd3967db616fd2
Zac Hansen <zhansen@pivotal.io>
2019-10-29 15:45:43 -0400

    added simple get/put

--- src/main/java/io/pivotal/datatx/datatxgemfirelocal/DatatxGemfireLocalApplication.java
+++ src/main/java/io/pivotal/datatx/datatxgemfirelocal/DatatxGemfireLocalApplication.java
@@ -1,7 +1,15 @@
 package io.pivotal.datatx.datatxgemfirelocal;
 
+import io.pivotal.datatx.datatxgemfirelocal.config.GeodeConfig;
+import org.apache.geode.cache.Region;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import static org.jgroups.util.Util.assertEquals;
+import static org.jgroups.util.Util.assertNotNull;
 
 @SpringBootApplication
 public class DatatxGemfireLocalApplication {
@@ -10,4 +18,13 @@ public class DatatxGemfireLocalApplication {
     SpringApplication.run(DatatxGemfireLocalApplication.class, args);
   }
 
+  @Bean
+  ApplicationRunner runner(@Qualifier(GeodeConfig.TEST_REGION) Region<String, String> testRegion) {
+    return args -> {
+      testRegion.put("testKey", "testValue");
+      String result = testRegion.get("testKey");
+      assertNotNull(result);
+      assertEquals("test", result);
+    };
+  }
 }
--- src/main/java/io/pivotal/datatx/datatxgemfirelocal/config/GeodeConfig.java
+++ src/main/java/io/pivotal/datatx/datatxgemfirelocal/config/GeodeConfig.java
@@ -2,9 +2,11 @@ package io.pivotal.datatx.datatxgemfirelocal.config;
 
 
 import io.pivotal.datatx.datatxgemfirelocal.security.TestSecurityManager;
+import org.apache.geode.cache.GemFireCache;
 import org.apache.geode.security.SecurityManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
+import org.springframework.data.gemfire.ReplicatedRegionFactoryBean;
 import org.springframework.data.gemfire.config.annotation.*;
 
 @CacheServerApplication(name = "ServerApplication")
@@ -14,9 +16,20 @@ import org.springframework.data.gemfire.config.annotation.*;
 @EnablePdx(readSerialized = true)
 public class GeodeConfig {
 
+  public static final String TEST_REGION = "test";
+
   @Bean
   SecurityManager testSecurityManager(Environment environment) {
     return new TestSecurityManager(environment);
+  }
+
+  @Bean(TEST_REGION)
+  ReplicatedRegionFactoryBean<String, String> testRegion(GemFireCache gemFireCache){
+    ReplicatedRegionFactoryBean<String,String> testRegion = new ReplicatedRegionFactoryBean<>();
+    testRegion.setCache(gemFireCache);
+    testRegion.setClose(false);
+    testRegion.setName(TEST_REGION);
+    return testRegion;
   }
 
 }
